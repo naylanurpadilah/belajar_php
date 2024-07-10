c9146f72f25278c27e53b7aea855b198b78d47df
naylanurpadilah <naylanurpadilah16@gmail.com>
2024-07-10 14:06:21 +0700

    bagian 7

---
+++ TipeDataNull.php
@@ -0,0 +1,29 @@
+<?php
+
+$name = "Nayla";
+$name = null;
+
+$age = null;
+
+echo "Name : ";
+echo $name;
+echo "\n";
+
+echo "Age: ";
+echo $Age;
+echo "\n";
+
+echo  "Is Name Null? : ";
+var_dump(is_null($name));
+echo "\n";
+
+$contoh = "Nayla";
+unset($contoh);
+
+$contoh = "Nayla";
+$contoh = null;
+
+var_dump(isset($contoh));
+
+
+
